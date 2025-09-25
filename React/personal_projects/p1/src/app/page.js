19339077e574416146dce4dfd8c5a64c88e63d13
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-09-25 00:08:39 -0300

    created code html

--- React/personal_projects/p1/src/app/page.js
+++ React/personal_projects/p1/src/app/page.js
@@ -2,8 +2,38 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div>
+        <h3>React JS</h3>
+      </div>
+      <div>
+        <h2>React JS</h2>
+        <h3>Biblioteca JavaScript para criar SPAs</h3>
+        <button>Site oficial</button>
+      </div>
+      <div>
+        <div>
+          <h2>Requisitos</h2>
+          <h3>Veja abaixo os requisitos minimos para aprender React JS:</h3>
+        </div>
+        <div>
+          <div>
+            <h4>HTML</h4>
+            <p>Aprender a estruturar os elementos da página com html</p>
+          </div>
+          <div>
+            <h4>CSS</h4>
+            <p>Aprender a estilizar os elementos da página com css</p>
+          </div>
+          <div>
+            <h4>JAVASCRIPT</h4>
+            <p>Aprender a linguagem utilizada pelo React JS</p>
+          </div>
+        </div>
+        <div>
+          Curso de introdução de React JS
+        </div>
+      </div>
+    </>
   );
 }
