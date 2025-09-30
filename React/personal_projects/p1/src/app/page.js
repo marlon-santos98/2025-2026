fc0ff3b1b3f8d711c845cf602792f0d2f58369f9
Marlon Santos <marlonadrianoteixeira@gmail.com>
2025-09-29 21:34:29 -0300

    project finished

--- React/personal_projects/p1/src/app/page.js
+++ React/personal_projects/p1/src/app/page.js
@@ -1,39 +1,39 @@
-import styles from "./page.module.css";
+import style from './page.module.css'
 
 export default function Home() {
   return (
     <>
       <div>
-        <h3>React JS</h3>
+        <h3 className={style.h3}>React JS</h3>
       </div>
-      <div>
+      <div className={style.conteudo_central}>
         <h2>React JS</h2>
         <h3>Biblioteca JavaScript para criar SPAs</h3>
         <button>Site oficial</button>
       </div>
-      <div>
+      <div className={style.requisitos}>
         <div>
           <h2>Requisitos</h2>
           <h3>Veja abaixo os requisitos minimos para aprender React JS:</h3>
         </div>
-        <div>
-          <div>
+        <div className={style.card_maior}>
+          <div className={style.card}>
             <h4>HTML</h4>
             <p>Aprender a estruturar os elementos da página com html</p>
           </div>
-          <div>
+          <div className={style.card}>
             <h4>CSS</h4>
             <p>Aprender a estilizar os elementos da página com css</p>
           </div>
-          <div>
+          <div className={style.card}>
             <h4>JAVASCRIPT</h4>
             <p>Aprender a linguagem utilizada pelo React JS</p>
           </div>
         </div>
-        <div>
-          Curso de introdução de React JS
-        </div>
       </div>
+      <footer className={style.footer}>
+          Curso de introdução de React JS
+      </footer>
     </>
   );
 }
